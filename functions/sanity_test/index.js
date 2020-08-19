ed6a19380770e06473ba315f7f20eea1f25d1f13
Willian Alves <willianvoliveiravx@gmail.com>
2020-08-19 13:17:33 -0300

    first commit

---
+++ functions/sanity_test/index.js
@@ -0,0 +1,34 @@
+const getOddHour = (aulas) => {
+    return aulas.filter(aulaItem => {
+
+       if (typeof aulaItem.hora !== "number" ) {
+           throw new Error("Property 'hora' must be a numeric type");
+       }
+
+       const isOdd = aulaItem.hora % 2 === 1;
+
+       if (isOdd) {
+           return aulaItem;
+       }
+
+    });
+}
+
+const list = [ 1,2,3,4,5,6,7,8,9,10,11,12,13 ];
+
+const aulas = [
+    {hora: 21},
+    {hora: 22},
+    {hora: 23},
+    {hora: 24},
+    {hora: 25},
+    {hora: 26},
+    {hora: 27},
+    {hora: 28},
+    {hora: 29}
+];
+
+const result = getOddHour(aulas);
+
+console.log(result);
+console.log(aulas);
